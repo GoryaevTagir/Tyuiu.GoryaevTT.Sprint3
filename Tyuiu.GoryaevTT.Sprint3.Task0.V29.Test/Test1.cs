07e23da1b05bc1cb4a51ab0d139ea6cd48421bf3
GoryaevTagir <Lobstera134@gmail.com>
2025-10-06 18:13:40 +0000

    Task0

---
+++ Tyuiu.GoryaevTT.Sprint3.Task0.V29.Test/Test1.cs
@@ -0,0 +1,19 @@
+﻿using Newtonsoft.Json.Linq;
+using Tyuiu.GoryaevTT.Sprint3.Task0.V29.Lib;
+namespace Tyuiu.GoryaevTT.Sprint3.Task0.V29.Test
+{
+    [TestClass]
+    public sealed class Test1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Class1 ds = new Class1();
+            double value = 0.5;
+            int startValue = 1;
+            int stopValue = 10;
+            double res = ds.GetSumSeries(value, startValue, stopValue);
+            Assert.AreEqual(2.194 , res);
+        }
+    }
+}
---
+++ Tyuiu.GoryaevTT.Sprint3.Task0.V29/Program.cs
@@ -0,0 +1,16 @@
+﻿using Tyuiu.GoryaevTT.Sprint3.Task0.V29.Lib;
+namespace Tyuiu.GoryaevTT.Sprint3.Task0.V29
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Class1 ds = new Class1();
+            double value = 0.5;
+            int startValue = 1;
+            int stopValue = 10;
+            double res = ds.GetSumSeries(value, startValue, stopValue);
+            Console.WriteLine(res);
+        }
+    }
+}
