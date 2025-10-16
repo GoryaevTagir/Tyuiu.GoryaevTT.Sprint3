4c03dfd18adb7d0d778e5569c0e69ca0f9715f82
GoryaevTagir <Lobstera134@gmail.com>
2025-10-16 11:10:58 +0000

    Task6

---
+++ Tyuiu.GoryaevTT.Sprint3.Task6.V28.Test/Test1.cs
@@ -0,0 +1,17 @@
+﻿using Tyuiu.GoryaevTT.Sprint3.Task6.V28.Lib;
+namespace Tyuiu.GoryaevTT.Sprint3.Task6.V28.Test
+{
+    [TestClass]
+    public sealed class Test1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Class1 ds = new Class1();
+            int a = 13;
+            int b = 19;
+            int res = ds.GetSumTheDivisors(a, b);
+            Assert.AreEqual(15, res);
+        }
+    }
+}
---
+++ Tyuiu.GoryaevTT.Sprint3.Task6.V28/Program.cs
@@ -0,0 +1,15 @@
+﻿using Tyuiu.GoryaevTT.Sprint3.Task6.V28.Lib;
+namespace Tyuiu.GoryaevTT.Sprint3.Task6.V28
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Class1 ds = new Class1();
+            int a = 13;
+            int b = 19;
+            int res = ds.GetSumTheDivisors(a, b);
+            Console.WriteLine(res);
+        }
+    }
+}
